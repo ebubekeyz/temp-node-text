61006b34bb0c4aa6983b8316dee38bb827f9dd15
ebubekeyz <ebubeofforjoe@gmail.com>
2022-08-07 18:36:48 +0100

    third commit

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-var http = require('http')
-
-var fs = require('fs')
-
-http
-.createServer(function (req, res) {
-    // const text = fs.readFileSync('./content/big.txt', 'utf8)
-    //res.end(text)
-
-    const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
-})
-
-.listen(5000)
+console.log('Welcome to Node Tutorial')
